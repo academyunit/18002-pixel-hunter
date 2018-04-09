839feed65e4177d60d2184ee603a41b67efd7227
k <admin@localhost>
2018-04-08 21:18:46 -0700

    Changes after CR

--- js/data/game-logic.test.js
+++ js/data/game-logic.test.js
@@ -1,21 +1,41 @@
 import {assert} from 'chai';
-import {checkScores, getMockAnswer, getTimer} from '../game-logic';
+import {checkScores, getTimer} from './game-logic';
 
 /**
- @todo: удалить текст отсюда
- Начните решение с описания тестовых случаев. Например:
-
- - Если игрок ответил меньше, чем на 10 вопросов, то игра считается не пройденной и функция должна вернуть -1;
- - Если игрок ответил на все вопросы и не быстро, и не медленно, и у него остались все жизни, то функция должна вернуть 1150 очков;
- - Комбинируйте разные условия ответов и кол-ва жизней, чтобы описать максимальное кол-во вариантов.
- - Реализуйте работу функции и убедитесь, что все тесты проходят.
+ * Получить сгенерированный mock-объект ответа.
+ *
+ * @param {Boolean} isCorrect
+ * @param {Number} time
+ * @return {Object}
  */
+export const getMockAnswer = (isCorrect, time) => {
+  return {
+    isCorrect,
+    time
+  };
+};
 
-describe(`Check scores`, () => {
-  /** Это тут надо или лишнее? Руками в тестах вбивать цифры или константы подтягивать откуда-то? */
-  const GAME_LIVES_BY_DEFAULT = 3;
+suite(`Check scores`, () => {
+  test(`check if answer object has a correct type`, () => {
+    const answers = [
+      getMockAnswer(true, 11),
+      `I'm a string!`,
+      `me too :)`,
+      getMockAnswer(true, 14),
+      getMockAnswer(true, 19),
+      getMockAnswer(true, 17),
+      getMockAnswer(true, 10),
+      getMockAnswer(true, 14),
+      getMockAnswer(true, 16),
+      getMockAnswer(false, 13)
+    ];
 
-  it(`should return -1 if questions answered < 10`, () => {
+    assert.throws(() => {
+      checkScores(answers);
+    }, `Given answer isn't an object!`);
+  });
+
+  test(`return -1 if answered < 10 questions`, () => {
     const answers = [
       getMockAnswer(true, 1),
       getMockAnswer(true, 2),
@@ -23,16 +43,30 @@ describe(`Check scores`, () => {
       getMockAnswer(true, 4),
       getMockAnswer(true, 5),
       getMockAnswer(true, 6),
-      getMockAnswer(true, 7),
-      getMockAnswer(true, 8),
-      getMockAnswer(true, 9),
+      getMockAnswer(true, 7)
+    ];
+
+    assert.equal(checkScores(answers), -1);
+  });
+
+  test(`return -1 if failed answers > 3`, () => {
+    const answers = [
+      getMockAnswer(true, 1),
+      getMockAnswer(true, 2),
+      getMockAnswer(true, 3),
+      getMockAnswer(true, 4),
+      getMockAnswer(true, 5),
+      getMockAnswer(true, 6),
+      getMockAnswer(false, 7),
+      getMockAnswer(false, 8),
+      getMockAnswer(false, 9),
       getMockAnswer(false, 10)
     ];
 
-    assert.equal(checkScores(answers, GAME_LIVES_BY_DEFAULT), -1);
+    assert.equal(checkScores(answers), -1);
   });
 
-  it(`should return 1150 if all questions are answered normally`, () => {
+  test(`should return 1150 if all questions are answered normally`, () => {
     const answers = [
       getMockAnswer(true, 11),
       getMockAnswer(true, 15),
@@ -43,15 +77,13 @@ describe(`Check scores`, () => {
       getMockAnswer(true, 10),
       getMockAnswer(true, 14),
       getMockAnswer(true, 16),
-      getMockAnswer(true, 11),
-      getMockAnswer(true, 12),
-      getMockAnswer(true, 13)
+      getMockAnswer(true, 11)
     ];
 
-    assert.equal(checkScores(answers, GAME_LIVES_BY_DEFAULT), 1150);
+    assert.equal(checkScores(answers), 1150);
   });
 
-  it(`should return 1400 if all questions are answered, 3 answered fast, 2 answered slow`, () => {
+  test(`should return 1150 if all questions are answered, 3 answered fast, 3 answered slow`, () => {
     const answers = [
       getMockAnswer(true, 5),
       getMockAnswer(true, 7),
@@ -60,57 +92,50 @@ describe(`Check scores`, () => {
       getMockAnswer(true, 19),
       getMockAnswer(true, 17),
       getMockAnswer(true, 10),
-      getMockAnswer(true, 14),
-      getMockAnswer(true, 16),
       getMockAnswer(true, 21),
       getMockAnswer(true, 22),
-      getMockAnswer(true, 13)
+      getMockAnswer(true, 23)
     ];
 
-    assert.equal(checkScores(answers, GAME_LIVES_BY_DEFAULT), 1400);
+    assert.equal(checkScores(answers), 1150);
   });
 
-  it(`should return 1050 if 10 questions are answered, only 1 live left`, () => {
+  test(`should return 1000 if 9 questions are answered and has only 2 lives`, () => {
     const answers = [
       getMockAnswer(true, 11),
       getMockAnswer(true, 12),
       getMockAnswer(true, 10),
       getMockAnswer(true, 14),
       getMockAnswer(true, 19),
-      getMockAnswer(false, 17),
+      getMockAnswer(true, 17),
       getMockAnswer(true, 10),
       getMockAnswer(true, 14),
       getMockAnswer(true, 16),
-      getMockAnswer(true, 15),
-      getMockAnswer(true, 14),
       getMockAnswer(false, 13)
     ];
 
-    assert.equal(checkScores(answers, 1), 1050);
+    assert.equal(checkScores(answers), 1000);
   });
 });
 
-describe(`Timer`, () => {
+suite(`Timer`, () => {
   it(`should decrease timer tick by 1 after tick() method is called`, () => {
     const timer = getTimer(10);
     timer.tick();
     timer.tick();
     timer.tick();
     timer.tick();
-    timer.tick();
 
-    assert.equal(timer.getTimeLeft(), 5);
+    assert.equal(timer.tick(), 5);
   });
 
-  it(`should notify when the timer ends`, () => {
+  test(`should notify when the timer ends`, () => {
     const timer = getTimer(4);
     timer.tick();
     timer.tick();
     timer.tick();
     timer.tick();
 
-    /** Что-то криво выглядит. Это то, о чем они просят в задании? :)
-     * Каким образом образом отлавливать окончание выполнения таймера (написал более подробно внутри getTimer() )? */
-    assert.throws(timer.tick.bind(timer), `I'm done with this!`);
+    assert.strictEqual(timer.tick(), -1, `I'm done with this!`);
   });
 });
